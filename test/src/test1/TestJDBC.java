675ec35a5e4015374ad01b3782ce0d2f5249b491
XYY <2323878901@qq.com>
2020-11-19 09:35:21 +0800

    查询

--- test/src/test1/TestJDBC.java
+++ test/src/test1/TestJDBC.java
@@ -2,6 +2,7 @@ package test1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class TestJDBC {
@@ -17,18 +18,28 @@ public class TestJDBC {
             //获取数据库连接对象
             Connection con=DriverManager.getConnection(dbURL,userName,userPwd);
             //删除
-            String sql="delete from account where name='赵四'";
+            //String sql="delete from account where name='赵四'";
             //插入
             //String sql="insert account values(3,'赵四','888')";
             //更新
             //String sql="update account set balance=500 where id=1";
+            //查询
+            String sql="select* from account";
             Statement stmt=con.createStatement();
-            int i =stmt.executeUpdate(sql);
-            if(i==1){
-                System.out.println("更新成功");
-            }else{
-                System.out.println("失败");
+//            int i =stmt.executeUpdate(sql);
+//            if(i==1){
+//                System.out.println("更新成功");
+//            }else{
+//                System.out.println("失败");
+//            }
+            ResultSet rs=stmt.executeQuery(sql); //返回查询的结果集
+            while(rs.next()){
+                String id=rs.getString("id");
+                String name=rs.getString("name");
+                String balance=rs.getString("balance");
+                System.out.println(id+"----"+name+"----"+balance);
             }
+
             stmt.close();
             con.close();
         }catch(Exception e) {
